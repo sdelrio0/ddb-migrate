985bd5b9bee0925ce7b33fa546bdc5e52a39ac2a
Sergio del Rio <sdelrio0@gmail.com>
2016-08-12 21:37:37 -0500

    chore(tests) Setup testing structure

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ddb-migrate
 ===========
-AWS DynamoDB Migration Tools for Node.js
+[![Travis build](https://img.shields.io/travis/sdelrio0/ddb-migrate.svg?style=flat)](https://travis-ci.org/sdelrio0/ddb-migrate)
+[![Codecov coverage](https://img.shields.io/codecov/c/github/sdelrio0/ddb.svg?style=flat)]()
+[![npm version](https://img.shields.io/npm/v/ddb-migrate.svg?style=flat)]()
+[![npm licence](https://img.shields.io/npm/l/ddb-migrate.svg?style=flat)]()
 
-[![Travis build](https://img.shields.io/travis/sdelrio0/ddb-migrate.svg?style=flat-square)](https://travis-ci.org/sdelrio0/ddb-migrate)
-[![Codecov coverage](https://img.shields.io/codecov/c/github/sdelrio0/ddb.svg?style=flat-square)]()
-[![npm version](https://img.shields.io/npm/v/ddb-migrate.svg?style=flat-square)]()
-[![npm licence](https://img.shields.io/npm/l/ddb-migrate.svg?style=flat-square)]()
+AWS DynamoDB Migration Tools for Node.js
 
 Introduction
 ============
--- __tests__/tests/core_tests.js
+++ __tests__/tests/core_tests.js
@@ -1,16 +1,19 @@
-var expect = require('chai').expect;
-var ddb = require('../../lib');
-
-describe('ddb', function() {
-  describe('list', function() {
-    it('returns an array', function() {
-      return ddb.ddb('listTables')
-        .then(res => {
-          return Promise.all([
-            expect(res).not.to.be.undefined,
-            expect(res.TableNames).to.be.an('array')
-          ]);
-        });
-    });
-  });
-});
+// import { expect } from 'chai';
+// import { myFn } from '../../lib';
+//
+// describe('ddb', function() {
+//   describe('list', function() {
+//     it('returns an array', function() {
+//       // return ddb.ddb('listTables')
+//       //   .then(res => {
+//       //     return Promise.all([
+//       //       expect(res).not.to.be.undefined,
+//       //       expect(res.TableNames).to.be.an('array')
+//       //     ]);
+//       //   });
+//
+//
+//       expect(myFn({hola: 1})).to.deep.equal({hola: 1, new: true});
+//     });
+//   });
+// });
