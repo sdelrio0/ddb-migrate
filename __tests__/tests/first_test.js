1ae5031ab3e3b6f9a6045c9d66178879a46f34cf
Sergio del Rio <sdelrio0@gmail.com>
2016-08-12 19:01:33 -0500

    test(test): Write test 2

--- __tests__/tests/first_test.js
+++ __tests__/tests/first_test.js
@@ -4,4 +4,8 @@ describe('first test', function() {
   it('should work', function() {
     expect(true).to.be.true;
   });
+  
+  it('should also work', function() {
+    expect(false).to.be.false;
+  });
 });
