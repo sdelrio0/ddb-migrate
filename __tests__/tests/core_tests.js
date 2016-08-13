7d5dfbc7935d624d58f761eef92adc499e7317f9
Sergio del Rio <sdelrio0@gmail.com>
2016-08-13 00:11:27 -0500

    chore(docker): Update to test DynamoDB with docker

--- __tests__/tests/core_tests.js
+++ __tests__/tests/core_tests.js
@@ -14,3 +14,31 @@ describe('dynamodb', function() {
     });
   });
 });
+//
+// import DdbLocal from 'ddb-local';
+//
+// describe('ddb', function () {
+//   this.timeout(10000);
+//
+//   var ddblocal = new DdbLocal();
+//
+//   before(function (done) {
+//     ddblocal.start(done);
+//   });
+//
+//   describe('list', function() {
+//     it('returns an empty array', function() {
+//       return ddb('listTables')
+//         .then(res => {
+//           return Promise.all([
+//             expect(res).not.to.be.undefined,
+//             expect(res.TableNames).to.be.an('array')
+//           ]);
+//         });
+//     });
+//   });
+//
+//   after(function (done) {
+//     ddblocal.stop(done);
+//   });
+// });
