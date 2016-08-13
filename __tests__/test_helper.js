8badfce74cbe34b8dc41cf8b966168b981973d48
Sergio del Rio <sdelrio0@gmail.com>
2016-08-13 00:49:41 -0500

    test(dynalite): add sessionToken so dynalite works in Travis

--- __tests__/test_helper.js
+++ __tests__/test_helper.js
@@ -1,9 +1,8 @@
 import dynalite from 'dynalite';
 
-const dynaliteServer = dynalite({path: './.dynamodb', createTableMs: 0});
+const dynaliteServer = dynalite({createTableMs: 0});
 const DYNALITE_PORT = 8000;
 
-// Listen on port 4567
 dynaliteServer.listen(DYNALITE_PORT, function(err) {
   if (err) throw err;
   
